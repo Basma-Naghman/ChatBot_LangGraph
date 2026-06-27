3af5b265ffdefe103042b2cecc3ae3b8769ecb8d
Basma Naghman <su92-bsaim-f23-048@superior.edu.pk>
2026-06-27 20:02:42 +0500

    first commit

---
+++ langraph_backend.py
@@ -0,0 +1,36 @@
+from langgraph.graph import StateGraph , START , END
+from typing import TypedDict , Annotated
+from langchain_core.messages import HumanMessage , SystemMessage , BaseMessage
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph.message import add_messages
+
+load_dotenv()
+llm = ChatGroq(
+    model="llama-3.1-8b-instant", 
+    temperature=0.7
+)
+
+class ChatState(TypedDict):
+    messages : Annotated[list[BaseMessage] , add_messages]
+
+def chat_node(state:ChatState):
+    #take user query from state
+    messages = state['messages']
+    
+    #send to llm
+    response = llm.invoke(messages)
+    
+    return {'messages':[response]}
+    
+        
+checkpointer  = MemorySaver()
+graph = StateGraph(ChatState)
+
+graph.add_node('chat_node',chat_node)
+
+graph.add_edge(START , 'chat_node')
+graph.add_edge('chat_node',END)
+
+chatbot = graph.compile(checkpointer=checkpointer)    
